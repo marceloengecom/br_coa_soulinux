9a0d68ffa09ad8e08a4d38b91bc3da2abe70ac96
Marcelo Costa <contato@soulinux.com>
2021-08-11 00:57:26 -0300

    .

--- __manifest__.py
+++ __manifest__.py
@@ -19,6 +19,7 @@
     ],
     'data': [
         'data/account_group.xml',
+        'data/br_chart_data.xml',
         'data/account.account.template.csv',
         'data/account_tax_template_data.xml',
     ],
